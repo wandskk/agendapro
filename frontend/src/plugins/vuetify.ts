66f7966795f6c7a8bff54d0158c8a83eaa084389
Wanderson Kenedy <42207590+wandskk@users.noreply.github.com>
2025-07-09 10:09:59 -0300

    chore: install Vuetify and configure theme

---
+++ frontend/src/plugins/vuetify.ts
@@ -0,0 +1,17 @@
+import "vuetify/styles/main.css";
+import { createVuetify } from "vuetify";
+import { aliases, mdi } from "vuetify/iconsets/mdi";
+import "@mdi/font/css/materialdesignicons.css";
+
+export const vuetify = createVuetify({
+  theme: {
+    defaultTheme: "light",
+  },
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+});
