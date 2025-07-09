99452109282a38f8305dd3cf17b69ebe49c35c94
Wanderson Kenedy <42207590+wandskk@users.noreply.github.com>
2025-07-09 10:12:27 -0300

    chore: install and configure Pinia

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,11 +2,14 @@
   <v-app>
     <v-main>
       <v-container>
-        <v-btn color="primary">Olá, Vuetify!</v-btn>
+        <v-btn @click="counter.increment">Contar: {{ counter.count }}</v-btn>
       </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script setup lang="ts">
+import { useCounterStore } from "./stores/counter";
+
+const counter = useCounterStore();
 </script>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,10 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import { vuetify } from "./plugins/vuetify";
+import { createApp } from 'vue';
+import App from './App.vue';
+import { vuetify } from './plugins/vuetify';
+import { createPinia } from 'pinia';
 
 const app = createApp(App);
+
+app.use(createPinia());
 app.use(vuetify);
-app.mount("#app");
+app.mount('#app');
